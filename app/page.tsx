fa8284a3f28962fd8e58c3e61ac4a292b9561b3a
Michael Peters Jr <michael@michaelpeterswa.com>
2024-11-29 13:49:45 -0800

    fix: basic scaffold

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <div className="-z-10 absolute inset-0 h-full w-full bg-white bg-[linear-gradient(to_right,#86b3ff33_1px,transparent_1px),linear-gradient(to_bottom,#86b3ff33_1px,transparent_1px)] bg-[size:24px_24px]" />
+      <div className="flex flex-col h-screen justify-center items-center text-[#2b64c7]">
+        <div className="text-6xl text-center">AlpineWorks</div>
+      </div>
+    </div>
+  );
+}
---
+++ lib/fonts.ts
@@ -0,0 +1,3 @@
+import { Major_Mono_Display } from "next/font/google";
+
+export const majormonodisplay = Major_Mono_Display({ weight: "400", subsets: ["latin"]});
